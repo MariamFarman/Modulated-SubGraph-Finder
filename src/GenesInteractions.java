1c851161ca3c62ace02af6713e9272796b8fe3f6
Mariam Farman <mariam_farman@hotmail.com>
2017-11-14 11:15:29 +0100

    Changes in Filename

---
+++ src/GenesInteractions.java
@@ -0,0 +1,21 @@
+public class GenesInteractions {
+	String column1;
+	String colmn2;
+
+	public String getColumn1() {
+		return column1;
+	}
+
+	public void setColumn1(String column1) {
+		this.column1 = column1;
+	}
+
+	public String getColmn2() {
+		return colmn2;
+	}
+
+	public void setColmn2(String colmn2) {
+		this.colmn2 = colmn2;
+	}
+
+}
